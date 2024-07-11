c83776616b1ef35a66e4fedee7768a0ca0b133dd
jobouillon <jbouillon@servicesepsilon.com>
2024-07-11 14:58:58 -0400

    code exemple

---
+++ src/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var variable = "hello";
+
+if(variable == "toto") 
+{
+    Console.WriteLine("Hello, World 1!");
+}
+
+double valeur = 2.0;
+
+var numbers = new List<int>();
+numbers.Add(valeur);
+
+Console.WriteLine(Add("Hello", "World 2!"));
+
+static string Add(string a, string b)
+{
+    return a + b;
+}
