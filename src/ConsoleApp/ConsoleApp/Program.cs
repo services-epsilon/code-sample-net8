8ebecc592244753cc2c2d7fcbd2a46ed5aba6656
jobouillon <90056299+jobouillon@users.noreply.github.com>
2024-12-11 12:19:03 -0500

    Update Program.cs
    ajouter un test .

--- src/ConsoleApp/ConsoleApp/Program.cs
+++ src/ConsoleApp/ConsoleApp/Program.cs
@@ -1,6 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 
-// # corriger les erreurs et faire pour que la console afficher "Hello, World 1!"
+// # 1
+// corriger les erreurs et faire pour que la console afficher "Hello, World 1!"
+
+using System;
 
 var variable = "hello"
 
@@ -9,8 +12,12 @@ if(variable == "toto")
     Console.WriteLine("Hello, World 1!);
 }
 
-// # faire une boucle et
+// # 2
+// faire une boucle et
 // afficher les chiffres en ordre croissant en format entier ex: "1" sans le .0
 // enlever le doublon 3.0
 List<double> valeurs = [2.0, 1.0, 4.0, 3.0, 5.0, 3.0];
 
+// # 3
+// Creer une classe Person et créer une fonction qui affiche "Bonjour, je suis Alice, et j'ai 30 years old."
+//var person = new Person { Name = "Alice", Age = 30 };
